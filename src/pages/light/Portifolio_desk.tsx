8154de6460e7f5bdb076ed3cf7503ddbfc82a6f9
Kesley Soares <kesleysoares@discente.ufg.br>
2025-07-22 12:46:37 +0000

    sessão de contato profissional

--- src/pages/light/Portifolio_desk.tsx
+++ src/pages/light/Portifolio_desk.tsx
@@ -1,3 +1,11 @@
+import NomeFoto from "../../components/portifolio_mob/nome_e_foto/nome_e_foto";
+
 export default function PortifolioDeskLight () {
-    return (<div>   BBBB </div>);
+    return (
+        <div className='div_portifolio_mobile_light'> 
+            <NomeFoto />
+    
+            <div className='contato_profissional'>aksdjap</div>
+    
+        </div>);
 }
--- src/pages/light/Portifolio_mobile.tsx
+++ src/pages/light/Portifolio_mobile.tsx
@@ -1,22 +1,63 @@
+import NomeFoto from '../../components/portifolio_mob/nome_e_foto/nome_e_foto';
+import Linkedin from '../../assets/Linkedin.png';
+import GitHub from '../../assets/Github.png';
+import Mail from '../../assets/Mail.png';
+import MapPoint from '../../assets/MapPoint.png';
 import './Portifolio_mobile.css';
-import FotoPerfil from "../../assets/Foto_Perfil.png"
 
 export default function PortifolioMobileLight () {
     return (
     <div className='div_portifolio_mobile_light'> 
+        <NomeFoto />
 
-        <div className="header_bg_mobile_light" >
-            <div style={{width: 800, height: 800, left: 0, top: 337, position: 'absolute', background: 'var(--Laranja-Opaco, #AD2831)', borderRadius: 9999}} />
-            <div style={{width: 1000, height: 1000, left: 429, top: 0, position: 'absolute', background: 'var(--Azul, #165BAB)', borderRadius: 9999}} />
-        </div>
+        <div className='contato_profissional'>
+    <h2>Contato Profissional</h2>
 
-        <div className='nome_e_foto_portifolio_mobile_light'>
-            <img src={FotoPerfil} />
-            <div>
-                <h1>Kesley Soares da Silva</h1>
-                <h2>Engenharia de Software | UFG</h2>
-            </div>
+    {/* --- Linkedin --- */}
+    <div className='formas_de_contato'>
+        <a href='https://www.linkedin.com/in/kesley-soares-da-silva-934413301/' target='_blank' rel='noopener noreferrer'>
+            <img src={Linkedin} alt="Logotipo do Linkedin" />
+        </a>
+        <div className='textos_contato_profissional'>
+            <p className='contatotitulo'>Linkedin</p>
+            <p className='contatodescricao'>linkedin.com/in/kesley-soares</p>
         </div>
+    </div>
+
+    {/* --- GitHub --- */}
+    <div className='formas_de_contato'>
+        <a href='https://github.com/kesleys' target='_blank' rel='noopener noreferrer'>
+            <img src={GitHub} alt="Logotipo do GitHub" />
+        </a>
+        <div className='textos_contato_profissional'>
+            <p className='contatotitulo'>GitHub</p>
+            <p className='contatodescricao'>github.com/kesleys</p>
+        </div>
+    </div>
+
+    {/* --- E-mail --- */}
+    <div className='formas_de_contato'>
+        <a href='mailto:kesleysoares@proton.me'>
+            <img src={Mail} alt="Ícone de e-mail" />
+        </a>
+        <div className='textos_contato_profissional'>
+            <p className='contatotitulo'>E-mail</p>
+            <p className='contatodescricao'>kesleysoares@proton.me</p>
+        </div>
+    </div>
+
+    {/* --- Localidade --- */}
+    <div className='formas_de_contato'>
+        <a href='https://www.google.com/maps/place/Goiânia,+GO' target='_blank' rel='noopener noreferrer'>
+            <img src={MapPoint} alt="Ícone de pino de mapa" />
+        </a>
+        <div className='textos_contato_profissional'>
+            <p className='contatotitulo'>Localidade</p>
+            <p className='contatodescricao'>Goiânia/GO - Brasil</p>
+        </div>
+    </div>
+
+</div>
 
     </div>);
 }
